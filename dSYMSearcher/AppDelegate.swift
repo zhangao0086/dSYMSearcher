ba00b766c2de79eea78a6f61649ef83b1c1f023c
Bannings <zhangao0086@gmail.com>
2017-07-31 12:19:18 +0800

    Added handle for NSWindowWillClose

--- dSYMSearcher/AppDelegate.swift
+++ dSYMSearcher/AppDelegate.swift
@@ -11,16 +11,19 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(windowWillClose), name: NSNotification.Name.NSWindowWillClose, object: nil)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    func windowWillClose() {
+        exit(0)
+    }
 
 }
 
